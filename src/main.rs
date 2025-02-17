a13f5ed4af4259fb23a25b27ef81eaf3bd62d108
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-17 23:55:32 +0800

    connect to x server.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use xcb::Connection;
+
+fn main() -> xcb::Result<()> {
+    let (conn, _) = Connection::connect(None)?;
+    let root = conn.get_setup().roots().nth(0).unwrap();
+
+    println!(
+        "Successfully connected to X server. Root window ID: {:?}",
+        root.root()
+    );
+
+    Ok(())
+}
